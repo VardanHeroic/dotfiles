16a022e656b9e57235361ab72f5e677a130c00df
VardanHeroic <tarayanvardan@gmail.com>
2024-10-04 20:39:39 +0400

    added wifi to waybar, added fzf to ranger

--- Scripts/changedesktopfiles.sh
+++ Scripts/changedesktopfiles.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-echo 0
+sed -i 's/ (Runtime)//g' /usr/share/applications/steam.desktop
 sed -i 's/viber.png/viber/g' /usr/share/applications/viber.desktop
 sed -i 's/Firefox Web Browser/Firefox/g' /usr/share/applications/firefox.desktop
 sed -i 's/WebCord/Discord/g' /usr/share/applications/webcord.desktop
+sed -i 's/Vesktop/Discord/g' /usr/share/applications/vesktop.desktop
+sed -i 's/Icon=vesktop/Icon=discord/' /usr/share/applications/vesktop.desktop
 sed -i 's/Telegram Desktop/Telegram/g' /usr/share/applications/org.telegram.desktop.desktop
 sed -i 's/Figma Linux/Figma/g' /usr/share/applications/figma-linux.desktop
 sed -i 's/Icon=nwg-look/Icon=theme-config/' /usr/share/applications/nwg-look.desktop
